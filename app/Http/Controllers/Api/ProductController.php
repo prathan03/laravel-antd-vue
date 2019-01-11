ed9e6fbc3f3f7b86ad0ab4f00adc78e0f5f929f7
Loren <mwl@live.com>
2019-01-11 13:42:47 +0800

    update

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+        return response()->json(json_decode('{"message":"","result":{"pageSize":null,"pageNo":null,"totalCount":57,"totalPage":null,"data":[]},"status":200,"timestamp":1534955098193}'));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -31,4 +31,7 @@ use Illuminate\Support\Facades\Route;
 Route::namespace('Api')->group(function () {
     Route::post('auth/login', 'AuthController@login');
     Route::post('auth/logout', 'AuthController@logout');
+    Route::get('user/info', 'UserController@info');
+    Route::get('role', 'RoleController@index');
+    Route::get('service', 'ServiceController@index');
 });
